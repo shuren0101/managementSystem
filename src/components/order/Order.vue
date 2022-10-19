ea0fdb4e62d86e5cb635414be2049c287c06b7db
殳刃 <2286560986@qq.com>
2022-10-19 15:20:46 +0800

    物流信息弹窗

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -40,15 +40,7 @@
           </template>
         </el-table-column>
       </el-table>
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="queryInfo.pagenum"
-        :page-sizes="[5, 10, 15]"
-        :page-size="queryInfo.pagesize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-      >
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryInfo.pagenum" :page-sizes="[5, 10, 15]" :page-size="queryInfo.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total">
       </el-pagination>
     </el-card>
     <!--修改地址对话框-->
@@ -79,95 +71,152 @@
 </template>
 
 <script>
-import cityData from './citydata.js'
-export default {
-  data() {
-    return {
-      queryInfo: {
-        query: '',
-        pagenum: 1,
-        pagesize: 10,
-      },
-      total: 0,
-      orderList: [],
-      addressVisible: false,
-      addressForm: {
-        address1: [],
-        address2: '',
-      },
-      addressFormRules: {
-        address1: [
-          {
+  import cityData from './citydata.js'
+  export default {
+    data() {
+      return {
+        queryInfo: {
+          query: '',
+          pagenum: 1,
+          pagesize: 10,
+        },
+        total: 0,
+        orderList: [],
+        addressVisible: false,
+        addressForm: {
+          address1: [],
+          address2: '',
+        },
+        addressFormRules: {
+          address1: [{
             required: true,
             message: '请选择省市区县',
             trigger: 'blur',
-          },
-        ],
-        address2: [
-          {
+          }, ],
+          address2: [{
             required: true,
             message: '请填写详细地址',
             trigger: 'blur',
-          },
-        ],
+          }, ],
+        },
+        cityData,
+        progressVisible: false, // 控制物流进度对话框是否显示
+        progressInfo: [], // 物流信息列表
+      }
+    },
+    created() {
+      this.getOrderList()
+    },
+    methods: {
+      async getOrderList() {
+        const { data: res } = await this.$http.get('orders', {
+          params: this.queryInfo,
+        })
+        if (res.meta.status !== 200) {
+          return this.$message.error('获取订单列表失败')
+        }
+        this.$message.success('获取订单列表成功!')
+        console.log(res.data.goods)
+        this.orderList = res.data.goods
+        this.total = res.data.total
       },
-      cityData,
-      progressVisible: false, // 控制物流进度对话框是否显示
-      progressInfo: [], // 物流信息列表
-    }
-  },
-  created() {
-    this.getOrderList()
-  },
-  methods: {
-    async getOrderList() {
-      const { data: res } = await this.$http.get('orders', {
-        params: this.queryInfo,
-      })
-      if (res.meta.status !== 200) {
-        return this.$message.error('获取订单列表失败')
-      }
-      this.$message.success('获取订单列表成功!')
-      console.log(res.data.goods)
-      this.orderList = res.data.goods
-      this.total = res.data.total
+      // 监听 pageSize 改变的事件
+      handleSizeChange(newSize) {
+        this.queryInfo.pagesize = newSize
+        this.getOrderList()
+      },
+      // 监听 当前页码值 改变的事件
+      handleCurrentChange(newPage) {
+        this.queryInfo.pagenum = newPage
+        this.getOrderList()
+      },
+      showBox() {
+        this.addressVisible = true
+      },
+      // 修改地址对话框的关闭事件
+      addressDialogClosed() {
+        this.$refs.addressFormRef.resetFields() // 清空表单
+        this.addressForm.address1 = []
+      },
+      // 展示物流进度的对话框
+      async showProgress() {
+        // const { data: res } = await this.$http.get('/kuaidi/804909574412544580')
+        // if (res.meta.status !== 200) {
+        //   return this.$message.error('获取物流信息失败！')
+        // }
+        // this.$message.success('获取物流信息成功！')
+        // console.log(res)
+        this.progressInfo = [{
+            time: '2018-05-10 09:39:00',
+            ftime: '2018-05-10 09:39:00',
+            context: '已签收,感谢使用顺丰,期待再次为您服务',
+            location: ''
+          },
+          {
+            time: '2018-05-10 08:23:00',
+            ftime: '2018-05-10 08:23:00',
+            context: '【北京市】北京海淀育新小区营业点派件员 顺丰速运95338正在为您派件',
+            location: ''
+          },
+          {
+            time: '2018-05-10 07:32:00',
+            ftime: '2018-05-10 07:32:00',
+            context: '快件到达 【北京海淀育新小区营业点】',
+            location: ''
+          },
+          {
+            time: '2018-05-10 02:03:00',
+            ftime: '2018-05-10 02:03:00',
+            context: '快件在【北京顺义集散中心】已装车,准备发往 【北京海淀育新小区营业点】',
+            location: ''
+          },
+          {
+            time: '2018-05-09 23:05:00',
+            ftime: '2018-05-09 23:05:00',
+            context: '快件到达 【北京顺义集散中心】',
+            location: ''
+          },
+          {
+            time: '2018-05-09 21:21:00',
+            ftime: '2018-05-09 21:21:00',
+            context: '快件在【北京宝胜营业点】已装车，准备发往 【北京顺义集散中心】',
+            location: ''
+          },
+          {
+            time: '2018-05-09 13:07:00',
+            ftime: '2018-05-09 13:07:00',
+            context: '顺丰速运 已收取快件',
+            location: ''
+          },
+          {
+            time: '2018-05-09 12:25:03',
+            ftime: '2018-05-09 12:25:03',
+            context: '卖家发货',
+            location: ''
+          },
+          {
+            time: '2018-05-09 12:22:24',
+            ftime: '2018-05-09 12:22:24',
+            context: '您的订单将由HLA（北京海淀区清河中街店）门店安排发货',
+            location: ''
+          },
+          {
+            time: '2018-05-09 21:36:04',
+            ftime: '2018-05-09 21:36:04',
+            context: '商品已下单',
+            location: ''
+          }
+        ]
+        this.progressVisible = true
+      },
     },
-    // 监听 pageSize 改变的事件
-    handleSizeChange(newSize) {
-      this.queryInfo.pagesize = newSize
-      this.getOrderList()
-    },
-    // 监听 当前页码值 改变的事件
-    handleCurrentChange(newPage) {
-      this.queryInfo.pagenum = newPage
-      this.getOrderList()
-    },
-    showBox() {
-      this.addressVisible = true
-    },
-    // 修改地址对话框的关闭事件
-    addressDialogClosed() {
-      this.$refs.addressFormRef.resetFields() // 清空表单
-      this.addressForm.address1 = []
-    },
-    // 展示物流进度的对话框
-    async showProgress() {
-      const { data: res } = await this.$http.get('/kuaidi/1106975712662')
-      if (res.meta.status !== 200) {
-        return this.$message.error('获取物流信息失败！')
-      }
-      this.$message.success('获取物流信息成功！')
-      console.log(res)
-      this.progressInfo = res.data
-      this.progressVisible = true
-    },
-  },
-}
+  }
 </script>
 <style lang="less" scoped>
-@import '../../plugins/timeline/timeline.css';
-@import '../../plugins/timeline-item/timeline-item.css';
-.el-cascader {
-  width: 100%;
-}
+  @import '../../plugins/timeline/timeline.css';
+  @import '../../plugins/timeline-item/timeline-item.css';
+
+  .el-cascader {
+    width: 100%;
+  }
 </style>
